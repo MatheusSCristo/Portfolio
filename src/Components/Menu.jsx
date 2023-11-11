288af9d8ac1c98e58196ddf2ab6c5f7e96c66491
Matheus <cristo.matheus@outlook.com>
2023-11-11 06:59:21 -0300

    Language change on menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -9,18 +9,26 @@ import { useSelector } from 'react-redux'
 const Menu = () => {
     const [active, setActive] = useState(false)
     const theme = useSelector((state) => state.config.theme)
+    const language=useSelector((state)=>state.config.language)
     return (
         <>
         {!active && <BiMenu onClick={()=>setActive(true)} size={30} color={theme==="dark"?Theme.darkTheme.white:Theme.lightTheme.black}/>}
         {active &&
             <S.Box theme={theme}>
             <S.Image src='/imgs/Close.svg' onClick={()=>setActive(false)}/>
-            <S.Container>
+            {language === "en" && <S.Container>
                 <S.text theme={theme}>Theme</S.text>
                 <ThemeButton />
                 <S.text theme={theme}>Language</S.text>
                 <Language />
-            </S.Container>
+            </S.Container>}
+            {language === "pt" && <S.Container>
+                <S.text theme={theme}>Tema</S.text>
+                <ThemeButton />
+                <S.text theme={theme}>Linguagem</S.text>
+                <Language />
+            </S.Container>}
+            
             </S.Box>
         }
     </>
