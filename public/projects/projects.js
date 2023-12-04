56fd30bf72238c2bf9961d5367a7744acf502d79
Matheus <cristo.matheus@outlook.com>
2023-12-04 14:41:09 -0300

    project your-message added

--- public/projects/projects.js
+++ public/projects/projects.js
@@ -13,6 +13,20 @@ const projects = [
             pt: "Este projeto foi feito utilizando React, Html, Css, JavaScript, para os dados meteorológicos foi utilizada uma integração API que entrega os dados de acordo com o local procurado."
         },
         languages: ["JS", "React", "CSS"]
+    },
+    {
+        title: {
+            en: "YourMessage ",
+            pt: "Your Message"
+        },
+        img: "/imgs/yourMessage.png",
+        git: "https://github.com/MatheusSCristo/YourMessage",
+        deploy: "https://matheusscristo.github.io/YourMessage/",
+        description: {
+            en: "This project is a social media page that allows communication with other people by message in real time, for the backend service Firebase was used which allows authentication and storage of user information.",
+            pt: "Este projeto é uma página de mídia social que permite a comunicação com outras pessoas por mensagem em tempo real, para o serviço backend foi utilizado o Firebase que permite autenticação e armazenamento de informações dos usuários."
+        },
+        languages: ["JS", "React", "CSS","Firebase","Styled-Components"]
     }
 
 ]
